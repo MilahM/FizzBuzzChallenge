daf0c8da9eaa609070124c8380cdd9ab74587ea9
Kamilah <k.muhammad95@gmail.com>
2023-01-25 15:25:34 -0600

    challenge complete

---
+++ FizzBuzzChallenge/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace FizzBuzzChallenge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+         Console.WriteLine(FizzBuzz(15));
+           
+        }
+
+        public static string FizzBuzz(int num)
+        {
+            var answer = "";
+
+            if (num % 3 == 0 && num % 5 == 0)
+            {
+                return "fizzbuzz";
+            }
+            if (num % 3 == 0)
+            {
+                return "fizz";
+            }
+            if (num % 5 == 0)
+            {
+                return "buzz";
+            }
+            return answer;
+
+        }
+    }
+}
